6df0a68cc3e7f4d09caf84484ea96aa79af82740
ssttkkl <huang.wen.long@hotmail.com>
2023-02-14 14:37:05 +0800

    添加当阻断时发送特定消息的功能

--- src/nonebot_plugin_access_control/config.py
+++ src/nonebot_plugin_access_control/config.py
@@ -1,4 +1,4 @@
-from typing import Literal, List
+from typing import Literal, List, Optional
 
 from nonebot import get_driver
 from pydantic import BaseSettings, Field
@@ -6,8 +6,12 @@ from pydantic import BaseSettings, Field
 
 class Config(BaseSettings):
     access_control_default_permission: Literal['allow', 'deny'] = 'allow'
+
     access_control_auto_patch_enabled: bool = False
     access_control_auto_patch_ignore: List[str] = Field(default_factory=list)
+
+    access_control_reply_on_permission_denied: Optional[str]
+    access_control_reply_on_rate_limited: Optional[str]
 
     class Config:
         extra = "ignore"
