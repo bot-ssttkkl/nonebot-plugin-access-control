39872878835ca714bed8b175d6f0186194ecffdc
ssttkkl <huang.wen.long@hotmail.com>
2023-02-20 11:28:05 +0800

    第一次启动时自动建表

--- src/nonebot_plugin_access_control/plugin_data.py
+++ src/nonebot_plugin_access_control/plugin_data.py
@@ -1,7 +1,15 @@
 from nonebot_plugin_datastore import get_plugin_data
+from nonebot_plugin_datastore.db import get_engine, post_db_init
 
 plugin_data = get_plugin_data()
 
 PluginModel = plugin_data.Model
+
+
+@post_db_init
+async def create_table():
+    async with get_engine().begin() as conn:
+        await conn.run_sync(lambda conn: plugin_data.metadata.create_all(conn))
+
 
 __all__ = ("plugin_data", "PluginModel")
