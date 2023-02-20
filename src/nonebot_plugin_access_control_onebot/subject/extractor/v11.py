6b8e7975a6a1a498f50634221ec5999603572939
ssttkkl <huang.wen.long@hotmail.com>
2023-02-20 10:31:26 +0800

    OneBotV11协议添加超级用户、群主、群管理主体

--- src/nonebot_plugin_access_control_onebot/subject/extractor/v11.py
+++ src/nonebot_plugin_access_control_onebot/subject/extractor/v11.py
@@ -1,4 +1,8 @@
+from typing import Optional
+
 from nonebot.adapters.onebot.v11 import Bot, Event
+from nonebot.adapters.onebot.v11.event import Sender
+from nonebot.permission import SuperUser
 
 from nonebot_plugin_access_control.subject import SubjectExtractor
 
@@ -14,18 +18,30 @@ class OneBotV11SubjectExtractor(SubjectExtractor[Bot, Event]):
         return platform == 'qq'
 
     def extract(self, bot: Bot, event: Event):
-        user_id = getattr(event, "user_id", None)
-        group_id = getattr(event, "group_id", None)
-
         li = []
 
+        user_id = getattr(event, "user_id", None)
         if user_id is not None:
             li.append(f"qq:{user_id}")
             li.append(f"onebot:{user_id}")
 
+            if SuperUser()(bot, event):
+                li.append("superuser")
+
+        group_id = getattr(event, "group_id", None)
         if group_id is not None:
             li.append(f"qq:g{group_id}")
             li.append(f"onebot:g{group_id}")
+
+            sender: Optional[Sender] = getattr(event, "sender", None)
+            if sender is not None:
+                if sender.role == 'owner':
+                    li.append(f"qq:group_owner")
+                    li.append(f"qq:g{group_id}.group_owner")
+
+                if sender.role == 'owner' or sender.role == 'admin':
+                    li.append(f"qq:group_admin")
+                    li.append(f"qq:g{group_id}.group_admin")
 
         li.append("qq")
         li.append("onebot")
